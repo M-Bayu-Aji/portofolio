a3d67f59e8b867b4fc66410afd8c7aeb9dc5e126
M-Bayu-Aji <muhammadbayuajisutisnaputra@smkwikrama.sch.id>
2024-07-17 20:01:02 +0700

    Portofolio 2

--- script.js
+++ script.js
@@ -12,3 +12,17 @@ document.addEventListener("click", function (e) {
     navbarNav.classList.remove("active");
   }
 });
+
+// Popup Img
+document.querySelectorAll('.projek img').forEach(Image => {
+  Image.onclick = () =>{
+    document.querySelector('.popup-img').style.display = 'block';
+    document.querySelector('.popup-img img').src = Image.getAttribute('src');
+    document.body.classList.add('no-scroll');
+  }
+
+  document.querySelector('.popup-img span').onclick = () =>{
+    document.querySelector('.popup-img').style.display = 'none';
+    document.body.classList.remove('no-scroll');
+  }
+})
